cca3819a39fc13430383f0e7803037dcc07ba813
Baymete <bora.aymete@gmail.com>
2013-08-02 16:48:40 +0300

    Add save_model to admin, modified gitignore

--- inventory/models.py
+++ inventory/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
 class Item(models.Model):
     serial_number = models.CharField(max_length=50)
-    item_number = models.CharField(max_length=50)
-    asset_number = models.CharField(max_length=10)
-    entry_date = models.DateField()
-    invoice_date = models.DateField()
-    invoice_number =models.CharField(max_length=20)
-    po_number = models.CharField(max_length=20)
+    item_number = models.CharField(max_length=50, blank=True)
+    asset_number = models.CharField(max_length=10, blank=True)
+    entry_date = models.DateField() # Add by signal
+    invoice_date = models.DateField(null=True, blank=True)
+    invoice_number =models.CharField(max_length=20, blank=True)
+    po_number = models.CharField(max_length=20, blank=True)
     warranty_status = models.BooleanField()
-    warranty_until = models.DateField()
-    current_owner = models.ForeignKey('Personnel')
-
+    warranty_until = models.DateField(null=True, blank=True)
+    current_owner = models.ForeignKey(User)
+    notes = models.TextField(blank=True)
+    
     def __unicode__(self):
         return self.serial_number
 
@@ -35,3 +39,6 @@ class Territory(models.Model):
 
     def __unicode__(self):
         return self.territory
+
+
+
