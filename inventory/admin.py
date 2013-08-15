46af85b7c0d8e3e001ccd7d3fc18e4a9e1566f5b
unknown <bora@.(none)>
2013-08-15 23:14:40 +0300

    Add route template and function to admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,49 +2,27 @@ from django.contrib import admin
 from django import forms
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Item, Personnel
 
 
-def make_warranty(modeladmin, request, queryset):
-    queryset.update(warranty_status=True)
-make_warranty.short_description = "Make warranty"
-
-def remove_warranty(modeladmin, request, queryset):
-    queryset.update(warranty_status=False)
-
-
-class RouteToUser(forms.Form):
-    _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
-    user = forms.ModelChoiceField(User.objects)
-
-def route_to_user(self, request, queryset):
-    form = None
-    
-    if 'apply' in request.POST:
-        form = self.RouteToUser(request.POST)
-
-        if form.is_valid():
-            user = form.cleaned_data['user']
-
-            count = 0
-            for item in queryset:
-                item.user = user
-                count += 1
-                
-            return HttpResponseRedirect(request.get_full_path())
-        
-        if not form:
-            form = self.RouteToUser(initial= {'_selected_action': request.POST.getList(admin.ACTION_CHECKBOX_NAME)})
-
-    return render_to_response('admin/route_to_user.html', 
-                                      {'users': queryset, 'user_form': form,})
-
 class ItemAdmin(admin.ModelAdmin):
-    actions = [make_warranty, remove_warranty, route_to_user]
+    actions = ['make_warranty', 'remove_warranty', 'route_to_user']
+    
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         obj.save()
+        
+    def make_warranty(modeladmin, request, queryset):
+        queryset.update(warranty_status=True)
+    make_warranty.short_description = "Make warranty"
+    
+    def remove_warranty(modeladmin, request, queryset):
+        queryset.update(warranty_status=False)
+    
+    def route_to_user(self, request, queryset):
+        return render_to_response('route.html', {'query':queryset})
 
 
 admin.site.register(Item, ItemAdmin)
